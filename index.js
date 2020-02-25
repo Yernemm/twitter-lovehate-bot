01bcf7e3eb5c88c6d3dc18ec4257955af3b454ee
Yernemm <yernemm@yernemm.xyz>
2020-02-25 10:30:42 +0000

    i t   w o r k s

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const config = require('./config.json');
 const pluralize = require('pluralize')
+const Twit = require('twit');
 const fs = require('fs');
 
 const words = fs.readFileSync('./words-20k.txt', 'utf8').replace(/\r/g,'').split('\n');
@@ -7,6 +8,8 @@ const intros = fs.readFileSync('./intros.txt', 'utf8').replace(/\r/g,'').split('
 
 const introsSing = intros[0].split('\n');
 const introsPlur = intros[1].split('\n');
+
+let T = new Twit(config);
 
 function generateRand()
 {
@@ -30,5 +33,38 @@ function cap(str)
     return str.charAt(0).toUpperCase() + str.substring(1);
 }
 
-for(let i = 0; i<20; i++)
-    console.log(generateRand());
+
+
+console.log(new Date().getTime());
+
+function nextHalfHour(){
+    let current = new Date().getTime()
+    current /= 1000; //seconds
+    current /= 60; //minutes
+    current /= 30; //half hour
+    current = Math.ceil(current);
+    current *= 30;
+    current *= 60;
+    current *= 1000;
+    if(current < new Date().getTime())
+        current += 1000 * 60 * 30;
+
+    return current;
+}
+
+setTimeout(()=>{
+    sendOne();
+    setInterval(()=>{
+        sendOne();
+    },1000 * 60 * 30);
+
+}, nextHalfHour() - new Date().getTime())
+
+function sendOne(){
+    T.post('statuses/update', { status: generateRand() }, function(err, data, response) {
+        console.log(data)
+      })
+}
+
+console.log(generateRand())
+console.log('we goin');
